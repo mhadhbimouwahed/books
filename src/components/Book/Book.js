db2d893be3a17c8c6446d0f89039fa167da31689
root <root@mouwahed.home>
2021-04-02 15:51:21 +0100

    added Book and BookList

---
+++ src/components/Book/Book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+      <h1>hey there</h1>
+    </div>
+  )
+}
+
+export default Book
---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Book from '../Book/Book'
+const BookList = () => {
+    return (
+        <div>
+            <Book></Book>
+        </div>
+    )
+}
+
+export default BookList
